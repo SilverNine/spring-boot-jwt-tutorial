2afd1bbeae211e64d1ab0f4a8b6c096e085ef48b
SilverNine <akasilvernine@gmail.com>
2021-07-31 15:53:53 +0900

    feat: add MethodArgumentNotValidExceptionHandler, 동영상 강의에는 없는 내용

---
+++ src/main/java/me/silvernine/tutorial/handler/MethodArgumentNotValidExceptionHandler.java
@@ -0,0 +1,66 @@
+package me.silvernine.tutorial.handler;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class MethodArgumentNotValidExceptionHandler {
+
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Error methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<org.springframework.validation.FieldError> fieldErrors = result.getFieldErrors();
+        return processFieldErrors(fieldErrors);
+    }
+
+    private Error processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
+        Error error = new Error(BAD_REQUEST.value(), "validation error");
+        for (org.springframework.validation.FieldError fieldError: fieldErrors) {
+            error.addFieldError(fieldError.getObjectName(), fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return error;
+    }
+
+    static class Error {
+        private final int status;
+        private final String message;
+        private List<FieldError> fieldErrors = new ArrayList<>();
+
+        Error(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void addFieldError(String objectName, String path, String message) {
+            FieldError error = new FieldError(objectName, path, message);
+            fieldErrors.add(error);
+        }
+
+        public List<FieldError> getFieldErrors() {
+            return fieldErrors;
+        }
+    }
+}
