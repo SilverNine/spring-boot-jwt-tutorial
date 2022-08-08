91f176c73abbef2db2856377a9d3814feaea735c
SilverNine <akasilvernine@gmail.com>
2022-08-09 08:58:23 +0900

    feat: cleaning imports

--- src/main/java/me/silvernine/tutorial/config/SecurityConfig.java
+++ src/main/java/me/silvernine/tutorial/config/SecurityConfig.java
@@ -7,9 +7,7 @@ import me.silvernine.tutorial.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -46,8 +44,8 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/h2-console/**"
-                ,"/favicon.ico"
-                ,"/error");
+                , "/favicon.ico"
+                , "/error");
     }
 
     @Bean
