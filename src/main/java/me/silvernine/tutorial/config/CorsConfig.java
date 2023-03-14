beee8600f4e26d65b77b4f882f428240291a1b68
SilverNine <akasilvernine@gmail.com>
2023-03-14 13:59:56 +0900

    feat: addAllowedOriginPattern 적용

--- src/main/java/me/silvernine/tutorial/config/CorsConfig.java
+++ src/main/java/me/silvernine/tutorial/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
-      config.addAllowedOrigin("*");
+      config.addAllowedOriginPattern("*");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
 
